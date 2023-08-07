1d42c26af2e10473cbafe2341eb5bbba6a9dc769
Levi Botelho <levibotelho@gmail.com>
2023-08-07 11:52:35 +0200

    feat: finish day 1 part 1

---
+++ 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AdventOfCode
+{
+	public static class One
+	{
+		public static void Run()
+		{
+            Console.WriteLine("Paste input and then press <Enter> three times...");
+
+            var lines = new List<string>();
+
+            while (lines.Count < 2 || (lines[^1] != "" || lines[^2] != ""))
+            {
+                var line = Console.ReadLine();
+                if (line != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            Console.WriteLine("Calculating...");
+
+            var calories = lines.Aggregate(new List<int> { 0 }, (a, x) =>
+            {
+                if (string.IsNullOrWhiteSpace(x))
+                {
+                    a.Add(0);
+                }
+                else
+                {
+                    var count = int.Parse(x);
+                    a[^1] += count;
+                }
+                return a;
+            });
+
+            Console.WriteLine($"Max calories: {calories.Max()}. Press any key to exit.");
+            Console.ReadLine();
+        }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter day...");
+var dayString = Console.ReadLine();
+int day;
+while (!int.TryParse(dayString, out day))
+{
+    Console.WriteLine("Invalid day.");
+    dayString = Console.ReadLine();
+}
+
+switch (day)
+{
+    case 1:
+        One.Run();
+        break;
+    default:
+        Console.WriteLine("Unsupported day. Press any key to exit.");
+        Console.ReadLine();
+        return;
+}
