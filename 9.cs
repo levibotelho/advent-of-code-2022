53a22474c8351130fa45781227bd2655caaa1c84
Levi Botelho <levibotelho@gmail.com>
2023-08-08 14:45:35 +0200

    feat: start on day 9 part 1

--- 9.cs
+++ 9.cs
@@ -1,3 +1,4 @@
+using System;
 using static AdventOfCode.Helpers;
 
 namespace AdventOfCode
@@ -7,6 +8,39 @@ namespace AdventOfCode
         public static void Run()
         {
             var lines = GetLines();
+            var movements = lines.Select(x => new Movement(x));
+        }
+
+        class Movement
+        {
+            public Movement(string line)
+            {
+                var split = line.Split(' ');
+                switch (split)
+                {
+                    case ["L", var distanceStr] when int.TryParse(distanceStr, out var distance):
+                        X = -distance;
+                        Y = 0;
+                        break;
+                    case ["R", var distanceStr] when int.TryParse(distanceStr, out var distance):
+                        X = distance;
+                        Y = 0;
+                        break;
+                    case ["U", var distanceStr] when int.TryParse(distanceStr, out var distance):
+                        X = 0;
+                        Y = distance;
+                        break;
+                    case ["D", var distanceStr] when int.TryParse(distanceStr, out var distance):
+                        X = 0;
+                        Y = -distance;
+                        break;
+                    default:
+                        throw new ArgumentException($"invalid line: {line}");
+                }
+            }
+
+            public int X { get; }
+            public int Y { get; }
         }
     }
 }
