0aef69b8105e5f5ec5da1d4a97bf221a41fc10ea
X <x>
2023-08-09 11:03:36 +0200

    feat: finish day 10

--- 10.cs
+++ 10.cs
@@ -7,7 +7,35 @@ namespace AdventOfCode
         public static void Run()
         {
             var lines = GetLines();
+            var registerHistory = GetRegisterHistory(lines);
             var signalCycles = new int[] { 20, 60, 100, 140, 180, 220 };
+            // -1 because the value is the signal after the cycle and we want the value during the cycle.
+            var signalSum = signalCycles.Select(x => registerHistory[x - 1] * x).Sum();
+            Console.WriteLine($"Signal sum: {signalSum}");
+
+            var screenLines = GetScreenLines(registerHistory);
+            Console.WriteLine("");
+            foreach (var line in screenLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> GetScreenLines(List<int> registerHistory)
+        {
+            const int screenWidth = 40;
+            return registerHistory
+                .Select((val, i) =>
+                {
+                    var pixel = i % screenWidth;
+                    return Math.Abs(val - pixel) <= 1 ? '#' : '.';
+                })
+                .Chunk(screenWidth)
+                .Select(x => new string(x));
+        }
+
+        static List<int> GetRegisterHistory(IEnumerable<string> lines)
+        {
             var registerVal = 1;
             var registerHistory = new List<int> { registerVal };
             foreach (var line in lines)
@@ -27,9 +55,7 @@ namespace AdventOfCode
                         throw new ArgumentException($"unhandled instruction: {line}");
                 }
             }
-            // -1 because the value is the signal after the cycle and we want the value during the cycle.
-            var signalSum = signalCycles.Select(x => registerHistory[x - 1] * x).Sum();
-            Console.WriteLine($"Signal sum: {signalSum}");
+            return registerHistory;
         }
     }
 }
