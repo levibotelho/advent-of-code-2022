adfba03d622fae0664aae601f82dff5e6da80b02
Levi Botelho <levibotelho@gmail.com>
2023-08-11 10:19:59 +0200

    feat: finish day 9

--- 9.cs
+++ 9.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using static AdventOfCode.Helpers;
 
 namespace AdventOfCode
@@ -9,14 +8,15 @@ namespace AdventOfCode
         public static void Run()
         {
             var lines = GetLines();
-            var movements = lines.Select(x => new Movement(x));
-            var tailPositionCount = GetTailPositionCount(movements);
-            Console.WriteLine($"Tail position count: {tailPositionCount}");
+            var movements = lines.Select(x => new Movement(x)).ToArray();
+            var tailPositionCount2 = GetTailPositionCount(new Rope(2), movements);
+            Console.WriteLine($"2-segment tail position count: {tailPositionCount2}");
+            var tailPositionCount10 = GetTailPositionCount(new Rope(10), movements);
+            Console.WriteLine($"10-segment tail position count: {tailPositionCount10}");
         }
 
-        static int GetTailPositionCount(IEnumerable<Movement> movements)
+        static int GetTailPositionCount(Rope rope, IReadOnlyCollection<Movement> movements)
         {
-            var rope = new Rope();
             foreach (var movement in movements)
             {
                 rope.ApplyMovement(movement);
@@ -59,30 +59,26 @@ namespace AdventOfCode
 
         class Rope
         {
-            private readonly List<Position> headTrace = new();
             private readonly List<Position> tailTrace = new();
+            private readonly Position[] segments;
 
-            public Rope()
+            public Rope(int segmentCount)
             {
-                headTrace.Add(Head);
-                tailTrace.Add(Tail);
+                segments = Enumerable.Repeat(new Position(0, 0), segmentCount).ToArray();
+                tailTrace.Add(segments[^1]);
             }
 
-            public Position Head { get; private set; } = new Position(0, 0);
-            public Position Tail { get; private set; } = new Position(0, 0);
-
-            public IReadOnlyList<Position> HeadTrace => headTrace;
             public IReadOnlyList<Position> TailTrace => tailTrace;
 
             public void ApplyMovement(Movement movement)
             {
                 if (movement.X != 0 && movement.Y == 0)
                 {
-                    Move(movement.X, (x) => Head.MoveX(x));
+                    Move(movement.X, (x) => segments[0].MoveX(x));
                 }
                 else if (movement.X == 0 && movement.Y != 0)
                 {
-                    Move(movement.Y, (x) => Head.MoveY(x));
+                    Move(movement.Y, (x) => segments[0].MoveY(x));
                 }
                 else
                 {
@@ -96,18 +92,26 @@ namespace AdventOfCode
                 var absSteps = sign * steps;
                 for (var i = 0; i < absSteps; i++)
                 {
-                    var lastHead = Head;
-                    Head = move(sign);
-
-                    var deltaX = Math.Abs(Head.X - Tail.X);
-                    var deltaY = Math.Abs(Head.Y - Tail.Y);
-                    if (deltaX > 1 || deltaY > 1)
+                    // Move head
+                    segments[0] = move(sign);
+                    for (var j = 1; j < segments.Length; j++)
                     {
-                        Tail = lastHead;
+                        var current = segments[j];
+                        var leader = segments[j - 1];
+                        var deltaX = leader.X - current.X;
+                        var deltaY = leader.Y - current.Y;
+                        if (Math.Abs(deltaX) <= 1 && Math.Abs(deltaY) <= 1)
+                        {
+                            // The current segment is touching its leader.
+                            continue;
+                        }
+
+                        var moveX = Math.Sign(deltaX);
+                        var moveY = Math.Sign(deltaY);
+                        segments[j] = current.MoveX(moveX).MoveY(moveY);
                     }
 
-                    headTrace.Add(Head);
-                    tailTrace.Add(Tail);
+                    tailTrace.Add(segments[^1]);
                 }
             }
         }
