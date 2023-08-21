507b16ec2909ac2a9ccccbf82a4247bd08a70cbe
Levi Botelho <levibotelho@gmail.com>
2023-08-21 14:42:41 +0200

    feat: finish day 20

--- 20.cs
+++ 20.cs
@@ -9,49 +9,20 @@ namespace AdventOfCode
         public static void Run()
         {
             var lines = GetLines();
-            var mixed = new LinkedList<int>(lines.Select(int.Parse));
-            var originalOrder = CreateOriginalOrder(mixed);
+            var mixed1 = Mix(lines, 1, 1);
+            var score1 = GetScore(mixed1);
+            Console.WriteLine(score1);
 
-            // This many shifts puts the value back in its original place. It is length - 1 because
-            // it equals the number of spaces between other items in which the current item can sit.
-            // e.g. if there are three items, item 1 can only go to 213 before ending up at 231 which
-            // is the same as 123.
-            var maxShiftCount = mixed.Count - 1;
+            var mixed2 = Mix(lines, 811589153, 10);
+            var score2 = GetScore(mixed2);
+            Console.WriteLine(score2);
+        }
 
-            foreach (var node in originalOrder)
-            {
-                // Convert all shifts to forward shifts for simplicity.
-                var shift = node.Value;
-                if (node.Value < 0)
-                {
-                    var addend = (int)Math.Ceiling((double)-shift / maxShiftCount) * maxShiftCount;
-                    Debug.Assert(addend != 0);
-                    shift += addend;
-                }
-
-                shift %= maxShiftCount;
-                if (shift == 0)
-                {
-                    continue;
-                }
-
-                var predecessor = node;
-                for (var i = 0; i < shift; i++)
-                {
-                    Debug.Assert(predecessor != null);
-                    predecessor = predecessor.Next ?? mixed.First;
-                }
-
-                Debug.Assert(predecessor != node && predecessor != node.Previous);
-
-                mixed.Remove(node);
-                Debug.Assert(predecessor != null);
-                mixed.AddAfter(predecessor, node);
-            }
-
+        static long GetScore(LinkedList<long> mixed)
+        {
             var j = int.MinValue;
             var current = mixed.First;
-            var score = 0;
+            var score = 0L;
             while (true)
             {
                 Debug.Assert(current != null);
@@ -73,12 +44,60 @@ namespace AdventOfCode
                 j++;
             }
 
-            Console.WriteLine(score);
+            return score;
         }
 
-        static LinkedListNode<int>[] CreateOriginalOrder(LinkedList<int> list)
+        static LinkedList<long> Mix(IEnumerable<string> lines, int encryptionKey, int mixCount)
         {
-            var originalOrder = new LinkedListNode<int>[list.Count];
+            var mixed = new LinkedList<long>(lines.Select(x => long.Parse(x) * encryptionKey));
+            var originalOrder = CreateOriginalOrder(mixed);
+
+            // This many shifts puts the value back in its original place. It is length - 1 because
+            // it equals the number of spaces between other items in which the current item can sit.
+            // e.g. if there are three items, item 1 can only go to 213 before ending up at 231 which
+            // is the same as 123.
+            var maxShiftCount = mixed.Count - 1;
+            for (var i = 0; i < mixCount; i++)
+            {
+                foreach (var node in originalOrder)
+                {
+                    // Convert all shifts to forward shifts for simplicity.
+                    var shift = node.Value;
+                    if (node.Value < 0)
+                    {
+                        var addend = (long)Math.Ceiling((double)-shift / maxShiftCount) * maxShiftCount;
+                        Debug.Assert(addend != 0);
+                        shift += addend;
+                        Debug.Assert(shift >= 0);
+                    }
+
+                    shift %= maxShiftCount;
+                    if (shift == 0)
+                    {
+                        continue;
+                    }
+
+                    var predecessor = node;
+                    for (var j = 0; j < shift; j++)
+                    {
+                        Debug.Assert(predecessor != null);
+                        predecessor = predecessor.Next ?? mixed.First;
+                    }
+
+                    Debug.Assert(predecessor != node && predecessor != node.Previous);
+
+                    mixed.Remove(node);
+                    Debug.Assert(predecessor != null);
+                    mixed.AddAfter(predecessor, node);
+                }
+            }
+
+            return mixed;
+        }
+
+        static LinkedListNode<long>[] CreateOriginalOrder(LinkedList<long> list)
+        {
+            var originalOrder = new LinkedListNode<long>[list.Count];
             var next = list.First;
             var i = 0;
             while (next != null)
